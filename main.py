4067a0b7e7e26d94c0f60a252fe877232d18891a
Parinay Rana <parinayrana@gmail.com>
2025-07-26 22:20:44 +0530

    e

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("AI Webscraper")
 
 url = st.text_input("Enter the website URL: ")
 
-if st.button("Scrape Website"):
-    st.write("Executing...")
+# if st.button("Scrape Website"):
+#     st.write("Executing...")
     
 ##
