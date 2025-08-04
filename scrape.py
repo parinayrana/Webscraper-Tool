e7629c08cc8fffbd615356955e942d757c977175
Parinay Rana <parinayrana@gmail.com>
2025-08-04 21:36:27 +0530

    Content split

--- scrape.py
+++ scrape.py
@@ -39,3 +39,9 @@ def clean_body_content(body_content):
     cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())  # to remove empty strings
 
     return cleaned_content
+
+
+def split_dom_content(dom_content, max_length = 6000):
+    return [
+        dom_content
+    ]
