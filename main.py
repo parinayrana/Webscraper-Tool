b23dfc6d4a013c0eca5d8d0cd57275810cae16f3
Parinay Rana <parinayrana@gmail.com>
2025-08-04 21:43:17 +0530

    main.py changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import (scrape_website,split_dom_content, clean_body_content, extract_body_content)
 
 st.title("AI Webscraper")
 
@@ -8,4 +8,14 @@ url = st.text_input("Enter the website URL: ")
 if st.button("Scrape Website"):
     st.write("Executing...")
     
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("VIEW DOM CONTENT"):
+        st.text_area("DOM COntent", cleaned_content, height=300)
+
+
 ###
